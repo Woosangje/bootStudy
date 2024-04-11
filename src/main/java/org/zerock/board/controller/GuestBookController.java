756e2c289d918b433e757dac5b4d4a16d0e34af8
10-5\kkw <dnedrbs3@naver.com>
2024-04-11 17:57:20 +0900

    no message

--- src/main/java/org/zerock/board/controller/GuestBookController.java
+++ src/main/java/org/zerock/board/controller/GuestBookController.java
@@ -1,19 +1,35 @@
 package org.zerock.board.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.service.GuestbookService;
 
 @Controller
 @RequestMapping("/guestbook") // http://localhost/guestbook/???
 @Log4j2
-public class GuestBookController {
+@RequiredArgsConstructor // 자동 주입을 위한 Annotation
+public class GuestbookController {
 
-    @GetMapping({"/","/list"})
-    public String list(){
-        log.info("GuestBookController.list() 메서드 실행");
-        return "/guestbook/list"; // list.html로 감
+    private final GuestbookService service ; //final로 선언
+
+    @GetMapping("/")
+    public String index(){
+
+        return "redirect:/guestbook/list";
+    }
+
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("list......" + pageRequestDTO);
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
 
     }
 }
--- src/main/java/org/zerock/board/dto/PageRequestDTO.java
+++ src/main/java/org/zerock/board/dto/PageRequestDTO.java
@@ -7,13 +7,24 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.util.List;
+
 @Builder
 @AllArgsConstructor
 @Data
-public class PageRequestDTO {
+public class PageRequestDTO<DTO, EN> {
 
+    //DTO리스트
+    private List<DTO> dtoList;
+
+    //총 페이지 번호
+    private int totalPage;
+
+    //현재 페이지 번호
     private int page;
+    //목록 사이즈
     private int size;
+
 
     public PageRequestDTO(){
         this.page = 1;
